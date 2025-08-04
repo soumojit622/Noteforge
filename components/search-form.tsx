8f90c0047a575914500023bb277d4d457e958f3c
soumojit622 <soumojitbanerjee22@gmail.com>
2025-08-04 13:36:58 +0530

    added search functionality

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -1,13 +1,18 @@
-import { Search } from "lucide-react"
+"use client";
 
-import { Label } from "@/components/ui/label"
+import { Search } from "lucide-react";
+
+import { Label } from "@/components/ui/label";
 import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarInput,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import { useQueryState } from "nuqs";
 
 export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+  const [search, setSearch] = useQueryState("search", { defaultValue: "" });
+
   return (
     <form {...props}>
       <SidebarGroup className="py-0">
@@ -17,12 +22,14 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
           </Label>
           <SidebarInput
             id="search"
-            placeholder="Search the docs..."
+            placeholder="Search your notes..."
             className="pl-8"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
         </SidebarGroupContent>
       </SidebarGroup>
     </form>
-  )
+  );
 }
